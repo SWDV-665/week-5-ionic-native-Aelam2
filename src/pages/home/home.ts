fd6857d4e70bbbd0abfdd43906b1d7f008042304
Alex Elam <aelam@grimco.com>
2020-03-28 19:01:33 -0500

    Add/Remove Grocery Item

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,76 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery";
+
+  items = [
+    {
+      name: 'Milk',
+      quantity: 1
+    },
+    {
+      name: 'Cheerioes',
+      quantity: 4
+    },
+    {
+      name: 'Chicken Breast',
+      quantity:5 
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
+  removeItem = (item, index) => {
+    console.log(`Removing item - ${item}, ${index}`, )
+    const toast = this.toastCtrl.create({
+      message: `Removing item - ${item.name} ... `,
+      duration: 3000
+    })
+  
+    toast.present();
+
+    this.items.splice(index, 1)
+  }
+  
+  addItem = () => {
+    console.log("Adding item")
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Grocery Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.items.push(data)
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 }
+
